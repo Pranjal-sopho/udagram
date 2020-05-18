08667e23c581f2931116c767a8646bfc0cead529
Pranjal-sopho <pranjalsharma.ism@gmail.com>
2020-05-18 20:27:27 +0530

    structural changes done

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udagrampranjal0
+export POSTGRES_PASSWORD=amazon.com
+export POSTGRES_HOST=udagrampranjal0.c9wo1siuavas.ap-south-1.rds.amazonaws.com
+export POSTGRES_DB=udagrampranjal0
+export AWS_BUCKET=udagram-pranjal-dev
+export AWS_REGION=ap-south-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
